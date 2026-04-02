0a6d6f7f24a25e1ba0fa1cb5b653ced3e48b678f
charlesluo06 <charlesluo06@gmail.com>
2026-04-01 23:03:54 -0700

    added practice component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Practice text1="Change Me!" text2="Click" text3="Props" />
-      </div>
+    </div>
   );
 };
 
