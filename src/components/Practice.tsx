265622c22a6b30844f702efa36fff89cea914c6c
charlesluo06 <charlesluo06@gmail.com>
2026-04-01 22:54:38 -0700

    added Practice component

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -1,9 +1,11 @@
 const Practice = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-ubi-pink p-10">
-      <div className="w-full bg-ubi-blue p-5 text-left">{props.text1}</div>
-        <div className="w-full bg-ubi-red-200 p-5 text-left">{props.text2}</div>
-        <div className="w-full bg-ubi-darkred-100 p-5 text-left">{props.text3}</div>
+    <div className="bg-ubi-pink flex w-3/4 items-center gap-1 p-10">
+      <div className="bg-ubi-blue w-full p-5 text-left">{props.text1}</div>
+      <div className="bg-ubi-red-200 w-full p-5 text-left">{props.text2}</div>
+      <div className="bg-ubi-darkred-100 w-full p-5 text-left">
+        {props.text3}
+      </div>
     </div>
   );
 };
