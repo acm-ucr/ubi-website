8c287995c1a4f86ca5123cc29cfb0484385d471e
Jacob <pokemine007craft@gmail.com>
2026-04-07 16:34:09 -0700

    Implement Desktop Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
 import UBILogo from "@/public/UBI_Logo.png";
+import { navbarLinks } from "@/data/navbarLinks";
 
 const Navbar = () => {
   return (
-    <div>
-      <Image src={UBILogo} alt="UBI Logo" width={100} height={100} />
-    </div>
+    <nav className="bg-ubi-red-100">
+      <div className="flex items-center justify-between px-4 text-white">
+        <Link href="/" className="flex items-center gap-4">
+          <div className="relative h-12 w-12 rounded-full bg-white">
+            <Image src={UBILogo} alt="logo" fill />
+          </div>
+          <div className="text-2xl">UBI</div>
+        </Link>
+        <div className="flex items-center gap-6 p-6 text-2xl">
+          {navbarLinks.map(({ link, name }, index) => (
+            <Link
+              key={index}
+              href={link}
+              className={"flex items-center justify-center px-4 text-white"}
+            >
+              {name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
   );
 };
 
