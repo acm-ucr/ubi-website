cc89c2aad92809eb69e836e6a01ea7cd974d6800
mdiep010 <mdiep010@ucr.edu>
2026-04-17 22:49:47 -0700

    Fixed 404 page comments

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -9,32 +9,30 @@ interface errorProps {
 
 const ErrorMessage = ({ toptext, bottext }: errorProps) => {
   return (
-    <>
-      <div className="relative flex min-h-[90vh] items-center justify-center overflow-hidden">
-        <Image
-          src={HeartPump}
-          alt="Red heart shaped pump decoration"
-          className="absolute top-[28%] -left-[13%] w-[37vw] -rotate-25"
-        />
+    <div className="relative flex min-h-[91vh] items-center justify-center overflow-hidden">
+      <Image
+        src={HeartPump}
+        alt="Red heart shaped pump decoration"
+        className="absolute top-[28%] -left-[13%] w-[37vw] -rotate-25"
+      />
 
-        <Image
-          src={HeartPump}
-          alt="Red heart shaped pump decoration"
-          className="absolute top-[28%] left-[76%] w-[37vw] scale-x-[-1] rotate-25"
-        />
+      <Image
+        src={HeartPump}
+        alt="Red heart shaped pump decoration"
+        className="absolute top-[28%] left-[76%] w-[37vw] scale-x-[-1] rotate-25"
+      />
 
-        <div className="-mt-[2%] flex flex-col items-center text-center font-extrabold">
-          <p className="text-ubiboard-red-300 text-[13vw]">{toptext}</p>
-          <p className="text-[3vw] text-[#151539]">{bottext}</p>
-          <Link
-            href="/"
-            className="bg-ubi-lightpink text-ubi-darkred-100 mt-[7%] rounded-3xl px-[6%] pt-[2%] pb-[2%] text-center text-[3vw] drop-shadow-xl transition duration-300 hover:drop-shadow-none"
-          >
-            Go home
-          </Link>
-        </div>
+      <div className="-mt-[2%] flex flex-col items-center text-center font-extrabold">
+        <p className="text-ubiboard-red-300 text-[13vw]">{toptext}</p>
+        <p className="text-ubi-navy text-[3vw]">{bottext}</p>
+        <Link
+          href="/"
+          className="bg-ubi-lightpink text-ubi-darkred-100 mt-[7%] rounded-3xl px-[6%] pt-[2%] pb-[2%] text-center text-[3vw] drop-shadow-xl transition duration-300 hover:drop-shadow-none"
+        >
+          Go home
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
