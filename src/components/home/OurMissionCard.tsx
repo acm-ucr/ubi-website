f458287c5977cc3a90743b88b5ad9c1f1b94f24c
azntin <djjtran@gmail.com>
2026-04-19 23:36:59 -0700

    removed comments and adjusted image type

--- src/components/home/OurMissionCard.tsx
+++ src/components/home/OurMissionCard.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import { StaticImageData } from "next/image";
 
-//interface for text and image props
 interface cardProps {
   text: string;
-  imageSrc: string;
+  imageSrc: StaticImageData;
 }
 
 const MissionCard = ({ text, imageSrc }: cardProps) => {
   return (
-    // desired flex box for organizing the entire card
     <div className="relative flex h-56 w-75 flex-col items-center">
-      {/* Background bubble behind content */}
       <div className="bg-ubi-tan absolute bottom-0 h-35 w-full rounded-4xl"></div>
-
-      {/* Actual displayed content */}
       <div className="absolute bottom-8 flex flex-col items-center">
         <Image src={imageSrc} alt="Mission image" />
         <p className="text-ubi-red-200 text-4xl font-bold">{text}</p>
