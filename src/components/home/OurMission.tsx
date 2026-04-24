4e2e6a23a1c48678266f6a155db105d4b1e0abfe
azntin <djjtran@gmail.com>
2026-04-23 17:35:22 -0700

    fixed component proportions and solved comments

--- src/components/home/OurMission.tsx
+++ src/components/home/OurMission.tsx
@@ -1,37 +1,15 @@
-import React from "react";
 import MissionCard from "./OurMissionCard";
-import Donate from "@/public/assets/Blood-Drop_Donate.svg";
-import Educate from "@/public/assets/Educate-Icon.svg";
-import Heart from "@/public/assets/Heart-Serve-Icon.svg";
-
-const missionCards = [
-  {
-    text: "Donate",
-    image: Donate,
-    imageSize: 100,
-  },
-  {
-    text: "Educate",
-    image: Educate,
-    imageSize: 100,
-  },
-  {
-    text: "Serve",
-    image: Heart,
-    imageSize: 160,
-    imageClassName: "-mb-8",
-  },
-];
+import { missionCards } from "@/data/missionCards";
 
 const OurMission = () => {
   return (
-    <div className="relative flex h-full w-5/9 p-5">
+    <div className="relative flex w-4/5 p-5">
       <div className="bg-ubi-lightpink h-full w-full rounded-4xl shadow-xl shadow-black/20">
         <div>
-          <p className="font-volkhov text-ubi-blue float-left px-6 pt-7 text-6xl font-semibold tracking-wider">
+          <p className="font-volkhov text-ubi-blue float-left px-6 pt-7 text-6xl font-bold tracking-wider">
             Our Mission
           </p>
-          <p className="text-ubi-blue px-6 pt-7 text-xl leading-relaxed font-medium">
+          <p className="text-ubi-blue px-6 pt-7 text-2xl leading-relaxed font-medium">
             To create a sustainable and equitable blood supply for all by
             continuously engaging and replenishing a network of young, diverse
             blood donor advocates. We aim to address the issue of the lack of
--- src/components/home/OurMissionCard.tsx
+++ src/components/home/OurMissionCard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Image from "next/image";
 import { StaticImageData } from "next/image";
 
@@ -16,8 +15,8 @@ const MissionCard = ({
   imageClassName,
 }: cardProps) => {
   return (
-    <div className="relative flex h-48 w-60 flex-col items-center">
-      <div className="bg-ubi-tan absolute bottom-0 h-28 w-full rounded-3xl"></div>
+    <div className="relative flex h-72 w-90 flex-col items-center">
+      <div className="bg-ubi-tan absolute bottom-0 h-42 w-full rounded-3xl"></div>
       <div className="absolute bottom-6 flex flex-col items-center">
         <Image
           src={imageSrc}
@@ -26,7 +25,7 @@ const MissionCard = ({
           height={imageSize}
           className={imageClassName}
         />
-        <p className="text-ubi-red-200 text-3xl font-bold">{text}</p>
+        <p className="text-ubi-red-200 text-5xl font-bold">{text}</p>
       </div>
     </div>
   );
