86a81fe4eb2007e3d57c61d66dc583f972700d35
mdiep010 <mdiep010@ucr.edu>
2026-04-24 14:37:59 -0700

    Created Still Interested component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Practice from "@/components/Practice";
+import Interest from "@/components/home/StillInterested";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Practice text1="Change Me!" text2="Click" text3="Props" />
-    </div>
+    <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <Practice text1="Change Me!" text2="Click" text3="Props" />
+      </div>
+
+      <div>
+        <Interest />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/home/StillInterested.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+const StillInterest = () => {
+  return (
+    <div className="relative items-center">
+      <p className="text-ubi-blue font-volkhov flex flex-col justify-center py-5 text-center text-[6.5vw] font-bold tracking-wider">
+        Still Interested?
+      </p>
+
+      <div className="bg-ubi-rose top-[90%] h-min w-min justify-self-center rounded-4xl drop-shadow-xl">
+        <p className="text-ubi-darkred-100 px-18 pt-7 pb-4 text-left text-[4vw] font-extrabold tracking-wide">
+          Visit our Parent Organization
+        </p>
+        <hr className="border-ubi-blue mx-18 w-[68vw] justify-self-center border-t-4 pb-5"></hr>
+        <p className="text-ubi-darkred-100 font-volkhov px-20 text-left text-[2vw] font-extrabold">
+          University Blood{" "}
+          <span className="text-ubiboard-red-300">Initiative</span>
+        </p>
+
+        <div className="flex">
+          <p className="text-ubi-blue w-[45vw] px-20 pt-2 pb-8 text-[2vw] text-wrap">
+            Empowers a next generation of diverse blood donors and advocates.
+          </p>
+          <Link
+            href="https://www.universitybloodinitiative.org/"
+            className="bg-ubi-tan text-ubi-blue border-ubi-blue -my-4 mb-15 justify-end rounded-3xl border-4 px-15 pt-5 pb-4 text-center text-[3vw] font-bold drop-shadow-xl"
+          >
+            Learn more
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StillInterest;
