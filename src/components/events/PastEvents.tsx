5700bb6fee9efd96e70b0d38889ae9db2cab3997
TheKingShark27 <tgoitia27@gmail.com>
2026-04-27 11:34:43 -0700

    I fixed width and padding and margin from issues in comments

--- src/components/events/PastEvents.tsx
+++ src/components/events/PastEvents.tsx
@@ -9,15 +9,15 @@ const PastEvents = () => {
         <p>PAST EVENTS</p>
       </div>
       <div className="flex flex-row">
-        <div className="mr-5 flex flex-1 flex-col items-center">
-          <Image className="w-4/5" src={Paint} alt="Paint Event Image" />
-          <div className="text-ubi-blue bg-ubi-pink w-4/5 overflow-visible rounded-br-4xl rounded-bl-4xl p-8 pt-10 pb-10 text-center text-5xl font-semibold shadow-xl">
+        <div className="flex flex-1 flex-col items-center">
+          <Image className="w-5/7" src={Paint} alt="Paint Event Image" />
+          <div className="text-ubi-blue bg-ubi-pink w-5/7 overflow-visible rounded-br-4xl rounded-bl-4xl p-8 py-10 text-center text-5xl font-semibold shadow-xl">
             <p>Paint, Petals, and Pints!</p>
           </div>
         </div>
-        <div className="ml-5 flex flex-1 flex-col items-center">
-          <Image className="w-4/5" src={Pumpkin} alt="Pumpkin Event Image" />
-          <div className="text-ubi-blue bg-ubi-pink w-4/5 overflow-visible rounded-br-4xl rounded-bl-4xl p-8 pt-10 pr-15 pb-10 pl-17 text-center text-5xl font-semibold tracking-tight shadow-xl">
+        <div className="flex flex-1 flex-col items-center">
+          <Image className="w-5/7" src={Pumpkin} alt="Pumpkin Event Image" />
+          <div className="text-ubi-blue bg-ubi-pink w-5/7 overflow-visible rounded-br-4xl rounded-bl-4xl p-8 py-10 text-center text-5xl font-semibold tracking-tight shadow-xl">
             <p>Pumpkin Painting w/ LifeStream</p>
           </div>
         </div>
