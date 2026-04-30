b39d441b18b66212c3d824c2eb8dd5f6d552c17b
mdiep010 <mdiep010@ucr.edu>
2026-04-30 11:00:01 -0700

    Created Future Projects & Collabs

---
+++ src/components/about/FutureProjects.tsx
@@ -0,0 +1,27 @@
+import FutureCard from "@/data/FutureProjectsCard";
+import { FutureProjects } from "@/data/OurFutureProjects";
+
+const FutureProject = () => {
+  return (
+    <div className="w-4/5 flex-col pt-8">
+      <div className="bg-ubi-red-100 h-[37.8vw] w-full rounded-4xl shadow-xl shadow-black/20">
+        <p className="text-ubi-white font-volkhov pt-10 text-center text-[4vw]">
+          Future Projects & Collaborations
+        </p>
+        <div className="flex flex-row justify-center gap-15 pt-15">
+          {FutureProjects.map((card) => (
+            <FutureCard
+              key={card.text}
+              imageSrc={card.image}
+              text={card.text}
+              imageSize={card.imageSize}
+              imageClassName={card.imageClassName ?? ""}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FutureProject;
---
+++ src/data/OurFutureProjects.ts
@@ -0,0 +1,24 @@
+import Campus from "@/public/assets/Blood_X_UCR.svg";
+import Syringe from "@/public/assets/Syringe.svg";
+import LifeStream from "@/public/assets/LifeStream_Blood.svg";
+
+export const FutureProjects = [
+  {
+    text: "UCR CAMPUS BLOOD DRIVE WEEK",
+    image: Campus,
+    imageSize: 215,
+    imageClassName: "pb-5",
+  },
+  {
+    text: "PHLEBOTOMY WEEK ON CAMPUS",
+    image: Syringe,
+    imageSize: 90,
+    imageClassName: "pb-5",
+  },
+  {
+    text: "MORE LIFESTREAM COLLABS",
+    image: LifeStream,
+    imageSize: 200,
+    imageClassName: "pt-3 pb-1",
+  },
+];
