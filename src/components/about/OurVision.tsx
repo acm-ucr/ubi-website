8f23e8e61359cdadbcf05d01e5e8ec5e45bd5a07
Elisa D'sa <edsa001@ucr.edu>
2026-05-04 13:58:00 -0700

    Updated OurVision compoenent

--- src/components/about/OurVision.tsx
+++ src/components/about/OurVision.tsx
@@ -5,19 +5,23 @@ const OurVision = () => {
   return (
     <div className="bg-ubi-tan flex w-4/5 items-center justify-center py-16">
       <div className="flex items-center gap-10">
-        <div className="relative w-9/20 h-97 shadow-[10px_8px_0px_#BEB1B1]">
-        <Image
-          src={OurVisionImg}
-          alt="OurVisionImg"
-          fill
-          className="object-cover"
+        <div className="relative h-97 w-9/20 shadow-[10px_8px_0px_#BEB1B1]">
+          <Image
+            src={OurVisionImg}
+            alt="OurVisionImg"
+            fill
+            className="object-cover"
           />
         </div>
 
         <div className="bg-ubi-pink flex w-1/2 flex-col justify-center rounded-[40px] p-10 shadow-[10px_8px_0px_#BEB1B1] backdrop-blur-[8px]">
-          <p className="font-volkhov text-ubi-blue text-5xl font-bold">Our Vision</p>
+          <p className="font-volkhov text-ubi-blue text-5xl font-bold">
+            Our Vision
+          </p>
 
-          <p className="text-ubi-red-200 mt-2 text-3xl font-semibold">UBI at UCR Goals</p>
+          <p className="text-ubi-red-200 mt-2 text-3xl font-semibold">
+            UBI at UCR Goals
+          </p>
 
           <p className="text-ubi-blue mt-4 text-2xl leading-relaxed">
             Our goal is to help bring stability to the blood supply by
