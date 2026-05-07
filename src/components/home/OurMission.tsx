e016dbc3f6f60f34c14402af6ffeec5d2e925a22
azntin <djjtran@gmail.com>
2026-05-06 22:41:13 -0700

    minor mobile responsiveness update to separate branch

--- src/components/home/OurMission.tsx
+++ src/components/home/OurMission.tsx
@@ -4,12 +4,12 @@ import { missionCards } from "@/data/missionCards";
 const OurMission = () => {
   return (
     <div className="relative flex w-4/5 p-5">
-      <div className="bg-ubi-lightpink h-full w-full rounded-4xl shadow-xl shadow-black/20">
+      <div className="bg-ubi-lightpink w-vh h-full rounded-4xl shadow-xl shadow-black/20 lg:w-full">
         <div>
-          <p className="font-volkhov text-ubi-blue float-left px-6 pt-7 text-7xl font-bold tracking-wider">
+          <p className="font-volkhov text-ubi-blue float-left px-6 pt-7 text-center text-6xl font-bold tracking-wider lg:text-left lg:text-7xl">
             Our Mission
           </p>
-          <p className="text-ubi-blue px-6 pt-7 text-2xl leading-relaxed font-medium">
+          <p className="text-ubi-blue px-6 pt-7 text-center text-2xl leading-relaxed font-medium lg:text-left">
             To create a sustainable and equitable blood supply for all by
             continuously engaging and replenishing a network of young, diverse
             blood donor advocates. We aim to address the issue of the lack of
@@ -17,7 +17,7 @@ const OurMission = () => {
             consistent donors and long-term advocates within their communities.
           </p>
         </div>
-        <div className="-mt-7 flex flex-row justify-around px-6 pb-6">
+        <div className="-mt-7 flex flex-col items-center justify-around px-6 pb-6 lg:flex-row">
           {missionCards.map((card) => (
             <MissionCard
               key={card.text}
