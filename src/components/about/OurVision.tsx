cfe426d48c4cd82f381f17d0032354626fb9af95
TheKingShark27 <tgoitia27@gmail.com>
2026-05-18 18:49:40 -0700

    Addressed comments, fixed redundant break points and fixed text sizes.

--- src/components/about/OurVision.tsx
+++ src/components/about/OurVision.tsx
@@ -3,9 +3,9 @@ import OurVisionImg from "@/public/assets/Vision-Photo.svg";
 
 const OurVision = () => {
   return (
-    <div className="bg-ubi-tan flex w-9/10 items-center justify-center pb-16 md:w-4/5 lg:w-4/5">
-      <div className="lg:flew-row flex flex-col items-center gap-10 md:flex-row">
-        <div className="relative h-50 w-4/5 shadow-[10px_8px_0px_#BEB1B1] md:h-97 md:w-9/20 lg:h-97 lg:w-9/20">
+    <div className="bg-ubi-tan flex w-9/10 items-center justify-center pb-16 md:w-4/5">
+      <div className="flex flex-col items-center gap-10 md:flex-row">
+        <div className="relative h-50 w-4/5 shadow-[10px_8px_0px_#BEB1B1] md:h-97 md:w-9/20">
           <Image
             src={OurVisionImg}
             alt="OurVisionImg"
@@ -14,16 +14,16 @@ const OurVision = () => {
           />
         </div>
 
-        <div className="bg-ubi-pink lg:1/2 flex w-9/10 flex-col justify-center rounded-[40px] p-10 shadow-[10px_8px_0px_#BEB1B1] md:w-1/2">
-          <p className="font-volkhov text-ubi-blue pb-2 text-6xl font-bold">
+        <div className="bg-ubi-pink flex w-9/10 flex-col justify-center rounded-[40px] p-10 shadow-[10px_8px_0px_#BEB1B1] md:w-1/2">
+          <p className="font-volkhov text-ubi-blue pb-2 text-5xl font-bold md:text-6xl">
             Our Vision
           </p>
 
-          <p className="text-ubi-red-200 mt-2 text-3xl font-bold">
+          <p className="text-ubi-red-200 mt-2 text-2xl font-bold md:text-3xl">
             UBI at UCR Goals
           </p>
 
-          <p className="text-ubi-blue mt-4 text-2xl">
+          <p className="text-ubi-blue mt-4 text-xl md:text-2xl">
             Our goal is to help bring stability to the blood supply by
             mobilizing young donors to donate blood and advocate for the cause
             as well. In doing this, we hope to build a future generation of
--- src/components/about/Statistics.tsx
+++ src/components/about/Statistics.tsx
@@ -16,7 +16,7 @@ const Statistics = ({
   bld_drv_text,
 }: StatisticsProps) => {
   return (
-    <div className="m-5 flex w-4/5 flex-col items-center justify-center rounded-4xl border border-t-2 border-r-3 border-b-7 border-l-3 border-[#c9aca3] bg-[#fdf9ed] p-4 shadow-md md:w-2/3 lg:w-4/5 lg:flex-row">
+    <div className="m-5 flex w-4/5 flex-col items-center justify-center rounded-4xl border border-t-2 border-r-3 border-b-7 border-l-3 border-[#c9aca3] bg-[#fdf9ed] p-4 shadow-md md:w-4/5 md:flex-row">
       <div className="flex flex-col items-center py-5 lg:px-15 lg:py-0">
         <div className="text-ubi-red-200 m-5 text-6xl font-black">
           {donation_num}
@@ -26,22 +26,22 @@ const Statistics = ({
         </div>
       </div>
 
-      <div className="block w-2/3 border-t-3 border-[#beb1b1] lg:hidden" />
-      <div className="hidden h-24 border-l-3 border-[#beb1b1] lg:block" />
+      <div className="block w-2/3 border-t-3 border-[#beb1b1] md:hidden" />
+      <div className="hidden h-24 border-l-3 border-[#beb1b1] md:block" />
 
-      <div className="flex flex-col items-center py-5 lg:px-10 lg:py-0">
+      <div className="flex flex-col items-center py-5 md:px-10 md:py-0">
         <div className="text-ubi-red-200 m-5 text-6xl font-black">
           {engage_num}
         </div>
-        <div className="text-ubi-blue pb-5 pl-10 text-4xl font-black">
+        <div className="text-ubi-blue pb-5 text-center text-4xl font-black">
           {engage_text}
         </div>
       </div>
 
-      <div className="block w-2/3 border-t-3 border-[#beb1b1] lg:hidden" />
-      <div className="hidden h-24 border-l-3 border-[#beb1b1] lg:block" />
+      <div className="block w-2/3 border-t-3 border-[#beb1b1] md:hidden" />
+      <div className="hidden h-24 border-l-3 border-[#beb1b1] md:block" />
 
-      <div className="flex flex-col items-center py-5 lg:py-0 lg:pl-15">
+      <div className="flex flex-col items-center py-5 md:py-0 md:pl-15">
         <div className="text-ubi-red-200 m-5 text-6xl font-black">
           {bld_drv_num}
         </div>
