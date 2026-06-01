4b1f9facbecb7c7b0262bad74640b183294322bd
TheKingShark27 <tgoitia27@gmail.com>
2026-05-31 22:16:14 -0700

    Added Animation to the Statistics and OutVision Component.

--- src/components/about/Statistics.tsx
+++ src/components/about/Statistics.tsx
@@ -1,3 +1,5 @@
+"use client";
+import {motion} from "motion/react"
 interface StatisticsProps {
   donation_num: string;
   donation_text: string;
@@ -17,38 +19,47 @@ const Statistics = ({
 }: StatisticsProps) => {
   return (
     <div className="m-5 flex w-4/5 flex-col items-center justify-center rounded-4xl border border-t-2 border-r-3 border-b-7 border-l-3 border-[#c9aca3] bg-[#fdf9ed] p-4 shadow-md md:w-4/5 md:flex-row">
-      <div className="flex flex-col items-center py-5 lg:px-15 lg:py-0">
+      <motion.div viewport={{ once: true, amount: 0.1 }}
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }} className="flex flex-col items-center py-5 lg:px-15 lg:py-0">
         <div className="text-ubi-red-200 m-5 text-6xl font-black">
           {donation_num}
         </div>
         <div className="text-ubi-blue pb-5 text-4xl font-black">
           {donation_text}
         </div>
-      </div>
+      </motion.div>
 
       <div className="block w-2/3 border-t-3 border-[#beb1b1] md:hidden" />
       <div className="hidden h-24 border-l-3 border-[#beb1b1] md:block" />
 
-      <div className="flex flex-col items-center py-5 md:px-10 md:py-0">
+      <motion.div viewport={{ once: true, amount: 0.1 }}
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }} className="flex flex-col items-center py-5 md:px-10 md:py-0">
         <div className="text-ubi-red-200 m-5 text-6xl font-black">
           {engage_num}
         </div>
         <div className="text-ubi-blue pb-5 text-center text-4xl font-black">
           {engage_text}
         </div>
-      </div>
+      </motion.div>
 
       <div className="block w-2/3 border-t-3 border-[#beb1b1] md:hidden" />
       <div className="hidden h-24 border-l-3 border-[#beb1b1] md:block" />
 
-      <div className="flex flex-col items-center py-5 md:py-0 md:pl-15">
+      <motion.div viewport={{ once: true, amount: 0.1 }}
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.4 }} className="flex flex-col items-center py-5 md:py-0 md:pl-15">
         <div className="text-ubi-red-200 m-5 text-6xl font-black">
           {bld_drv_num}
         </div>
         <div className="text-ubi-blue pb-5 text-4xl font-black">
           {bld_drv_text}
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
