61624a404c1ba2539bf2afd140f2b17319de1c67
JeffreyXia05 <jeffx2016@gmail.com>
2026-06-01 21:53:48 -0700

    small change

--- src/components/about/OurVision.tsx
+++ src/components/about/OurVision.tsx
@@ -8,8 +8,9 @@ const OurVision = () => {
     <div className="bg-ubi-tan flex w-9/10 items-center justify-center pb-16 md:w-4/5">
       <div className="flex flex-col items-center gap-10 md:flex-row">
         <motion.div
-          initial={{ filter: "blur(100px)" }}
+          initial={{ filter: "blur(10px)" }}
           animate={{ filter: "none" }}
+          transition={{ duration: 0.8 }}
           className="relative h-50 w-4/5 shadow-[10px_8px_0px_#BEB1B1] md:h-97 md:w-9/20"
         >
           <Image
